8bc40eaff7e01346419ceca0b92fda63f8f524bd
BagasApg <bagasapg@gmail.com>
2023-05-19 17:38:40 +0700

    changed color to black, redo yesterday today time

--- index.php
+++ index.php
@@ -8,8 +8,14 @@ $chats = [];
 while ($chatAssoc = mysqli_fetch_object($result)) {
     $chats[] = $chatAssoc;
 }
-$date = date('M', time());
-// dd($date);
+// $date = date('j', strtotime($chats[0]->sent_at));
+// $date = 17;
+// if($date == intval(date('j', time()))-1){
+//     dd("Kemarin " . $date);
+
+// } else {
+//     dd("Sekarang " . $date);
+// }
 ?>
 
 <!DOCTYPE html>
@@ -19,28 +25,49 @@ $date = date('M', time());
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Discord | Chats</title>
+    <title>Connext | Chats</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <div class="wrapper">
-
-        <?php foreach ($chats as $chat) : ?>
-            <div class="chat-bubble">
-                <div class="bubble-head">
-                    <p class="bubble-user"><?= $chat->sent_from ?></p>
-                    <p class="bubble-sent-at"><?= date('M j Y g:i A', strtotime($chat->sent_at)) ?></p>
+        <div class="header">
+            Chat
+        </div>
+        <div class="chats">
+            <?php if($result->num_rows > 0): ?>
+            <?php foreach ($chats as $chat) : ?>
+                <?php 
+                $sent_at = date('g:i A', strtotime($chat->sent_at));
+                if(intval(date('j', strtotime($chat->sent_at))) == intval(date('j', time()))-1){
+                    $when = "Yesterday at " . $sent_at;
+                }else if(intval(date('j', strtotime($chat->sent_at))) == intval(date('j', time()))) {
+                    $when = "Today at " . $sent_at;
+                }else {
+                    $when = date('l m Y g:i A', strtotime($chat->sent_at));
+                } ?>
+                <div class="chat-bubble">
+                    <div class="bubble-head">
+                        <p class="bubble-user"><?= $chat->sent_from ?></p>
+                        <p class="bubble-sent-at"><?= $when ?></p>
+                    </div>
+                    <p class="bubble-text"><?= $chat->chat ?></p>
                 </div>
-                <p class="bubble-text"><?= $chat->chat ?></p>
-            </div>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+            <?php else: ?>
+                <div class="no-chats">
+                    
+                    <p>It is empty for now.</p>
+                    <p>Start chatting!</p>
+                </div>
+            <?php endif; ?>
+        </div>
 
         <div class="texting-container">
             <form action="send_chat.php" method="post">
                 <div class="texting-group">
 
-                    <input class="texting-input" type="text" name="chat">
+                    <input class="texting-input" type="text" name="chat" placeholder="Message here..">
                     <input type="hidden" name="sent_from" id="sent_from" value="ennie">
                     <button class="texting-button" type="submit" name="send">
                         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search">
--- send_chat.php
+++ send_chat.php
@@ -6,6 +6,10 @@ if (isset($_POST["send"])) {
     $chat = $_POST["chat"];
     $sent_from = $_POST["sent_from"];
 
+    if(empty($chat)){
+        redirect("index.php");
+        die;
+    }
     $result = mysqli_query($conn, "INSERT INTO chats(chat, sent_from, sent_at) VALUES('$chat', '$sent_from',now())");
     redirect("index.php");
 } else {
